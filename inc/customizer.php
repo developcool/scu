bcc02eb92987d4629fe7f3b6e4889274bf869af7
Nikolay Sinelnikov <nikola.sinelnik@gmail.com>
2019-01-03 02:50:19 +0300

    Create Theme SCU

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * scu-ce Theme Customizer
+ *
+ * @package scu-ce
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function scu_ce_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'scu_ce_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'scu_ce_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'scu_ce_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function scu_ce_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function scu_ce_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function scu_ce_customize_preview_js() {
+	wp_enqueue_script( 'scu-ce-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'scu_ce_customize_preview_js' );
